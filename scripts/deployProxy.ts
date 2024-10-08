ad0419db1ef413005b9ad30a17c9a47bb4286cb1
Igor Senych <igor.senych@cloudwalk.io>
2024-10-08 21:00:54 +0300

    feat: update scripts

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -2,13 +2,18 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const CONTRACT_NAME: string = ""; // TBD: Enter contract name
+  const TOKEN_ADDRESS: string = ""; // TBD: Enter token contract address
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
-  const proxy = await upgrades.deployProxy(factory);
+  const proxy = await upgrades.deployProxy(
+    factory,
+    [TOKEN_ADDRESS],
+    { kind: "uups" }
+  );
 
-  await proxy.deployed();
+  await proxy.waitForDeployment();
 
-  console.log("Proxy deployed to:", proxy.address);
+  console.log("Proxy deployed to:", proxy.target);
 }
 
 main().then().catch(err => {
