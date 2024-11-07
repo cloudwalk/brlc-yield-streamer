07015a11d50061492907bdd3911a9264ad774351
ihoroleksiienko <ihor.oleksiienko@cloudwalk.io>
2024-11-07 18:02:35 +0200

    chore: sync files with all repos

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -1,8 +1,8 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const CONTRACT_NAME: string = ""; // TBD: Enter contract name
-  const TOKEN_ADDRESS: string = ""; // TBD: Enter token contract address
+  const CONTRACT_NAME: string = "YieldStreamerV2"; // TODO: Enter contract name
+  const TOKEN_ADDRESS: string = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // TODO: Enter token contract address
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
   const proxy = await upgrades.deployProxy(
@@ -13,7 +13,7 @@ async function main() {
 
   await proxy.waitForDeployment();
 
-  console.log("Proxy deployed to:", proxy.target);
+  console.log("Proxy deployed to:", await proxy.getAddress());
 }
 
 main().then().catch(err => {
--- scripts/upgradeProxy.ts
+++ scripts/upgradeProxy.ts
@@ -1,12 +1,12 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const CONTRACT_NAME: string = ""; // TBD: Enter contract name
-  const PROXY_ADDRESS: string = ""; // TBD: Enter proxy address
+  const CONTRACT_NAME: string = "YieldStreamerV2"; // TODO: Enter contract name
+  const PROXY_ADDRESS: string = "0x9d4454B023096f34B160D6B654540c56A1F81688"; // TODO: Enter proxy address
 
   // Upgrade options:
-  // unsafeAllowRenames: true
-  // unsafeSkipStorageCheck: true
+  // - unsafeAllowRenames: true
+  // - unsafeSkipStorageCheck: true
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
   await upgrades.upgradeProxy(PROXY_ADDRESS, factory);
