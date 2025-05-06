21096c7bb44e06e31ae0fe8f38c27c53a258770c
Igor Senych <72256997+igorsenych-cw@users.noreply.github.com>
2025-05-06 14:25:02 +0300

    build: contract-upgrade (#29)
    * fix: contract storage `__gap`
    * build: upgrade `YieldStreamer` contract in Testnet
    * build: upgrade `YieldStreamer` contract in Mainnet

--- contracts/YieldStreamer.sol
+++ contracts/YieldStreamer.sol
@@ -1203,5 +1203,5 @@ contract YieldStreamer is
      * @dev This empty reserved space is put in place to allow future versions
      * to add new variables without shifting down storage in the inheritance chain
      */
-    uint256[43] private __gap;
+    uint256[42] private __gap;
 }
